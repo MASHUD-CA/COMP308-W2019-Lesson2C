ab3e777f4dc4774077526083928193b865cc1111
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-11-02 22:56:57 -0400

    Added arrow function (=>)

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,6 +1,7 @@
 //IIFE -- Immediately Invoked Funciotn Expressoin
 
 (function() {
+  /*
   function Start() {
     let student: objects.Student;
 
@@ -8,6 +9,12 @@
     student.saysHello();
     student.studies();
   }
+*/
 
-  window.addEventListener("load", Start);
+  window.addEventListener("load", ()=> {
+    let student: objects.Student;
+
+    student = new objects.Student(20, "Ahmed", "DB110");
+    student.saysHello();
+    student.studies(););
 })();
